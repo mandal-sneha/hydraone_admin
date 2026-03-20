7440ec9fe4ed83633e07967a35c9540badfad848
mandal-sneha <mandal.sneha174@gmail.com>
2026-03-20 20:28:33 +0530

    Add District and Municipality pages with routing and search functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./components/pages/landingpage/Home";
 import { LoginPage } from "./components/pages/auth/LoginPage";
 import Dashboard from "./components/pages/dashboard/Dashboard";
+import { DistrictPage } from "./components/pages/dashboard/DistrictPage";
+import { MunicipalityPage } from "./components/pages/dashboard/MunicipalityPage";
 import { HouseDetails } from "./components/pages/housedetails/HouseDetails";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -18,6 +20,24 @@ function App() {
           element={
             <ProtectedRoute>
               <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/districts"
+          element={
+            <ProtectedRoute>
+              <DistrictPage />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/district/:districtSlug"
+          element={
+            <ProtectedRoute>
+              <MunicipalityPage />
             </ProtectedRoute>
           }
         />
